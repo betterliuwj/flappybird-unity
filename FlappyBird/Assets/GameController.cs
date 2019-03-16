abb9b69779ddeafdcc772d8f3fe19b67d32c7991
Wenjing <wenjingliu123@gmail.com>
2019-03-15 23:27:14 -0400

    added score and game over text and game controller for it

---
+++ FlappyBird/Assets/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public static GameController instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        // active the gameover object
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
